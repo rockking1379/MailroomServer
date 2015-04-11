a634085cd3e06cd4e0d7993fa3653b2da8e593a8
James <sitzja@grizzlies.adams.edu>
2015-04-11 00:19:02 -0600

    working on users
    working on gettings Users functional in API. mostly trying to figure out
    where to put data needed for specific methods. POST can use body, all
    others need to use headers

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var packages = require('./routes/package');
+var packages = require('./routes/packages');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/package', packages);
+app.use('/packages', packages);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,84 +1,91 @@
 var express = require('express');
 var router = express.Router();
 var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('B:\\Programming\\NodeJS\\rest\\data\\newmail.db');
+var dataDb = new sqlite3.Database('B:\\Programming\\NodeJS\\rest\\data\\newmail.db');
+var sessionDb = new sqlite3.Database('B:\\Programming\\NodeJS\\rest\\data\\session.db');
 var string = require('string');
 
-/* GET users listing. */
-router.get('/', function (req, res) {
-    db.all('SELECT user_id, user_name, first_name, last_name, administrator FROM Users WHERE active=1', function (err, rows) {
-        if (err) {
-            res.send('database error');
+/* Common Methods */
+function verifySession(sessionId, callback){
+    sessionDb.get('SELECT valid_until AS expire FROM Sessions WHERE session_id=?', [sessionId], function(err, row){
+        if(err){
             console.error(err);
+            callback(false);
         }
-        else {
-            res.send(JSON.stringify(rows));
+        else{
+            var cTime = new Date();
+            var eTime = new Date(row.expire);
+
+            callback(cTime < eTime);
         }
     });
-});
+}
+function returnError(res, message){
+    res.send(JSON.stringify({'success':0, 'message':message}));
+}
+function returnSuccess(res, data){
+    res.send(JSON.stringify({'success':1,'data':data}));
+}
 
-router.get('/:id', function (req, res) {
-    var params = [Number(req.params.id)];
-    db.all('SELECT user_id, user_name, first_name, last_name, administrator FROM Users WHERE user_id=?', params, function (err, row) {
-        if (err) {
-            res.send('database error');
-            console.error(err);
-        }
-        else {
-            if(row == undefined){
-                var response = {};
-                res.send(JSON.stringify(response));
-            }
-            else {
-                res.send(JSON.stringify(row));
-            }
-        }
+/* HTTP GET Actions */
+router.get('/inactive', function (req, res){
+    verifySession(req.headers.sessionid, function(valid){
+       if(valid){
+           dataDb.all('SELECT user_id, user_name, first_name, last_name, administrator FROM Users WHERE active=0', function(err, rows){
+              if(err){
+                  returnError(res, 'database error');
+              }
+               else{
+                  returnSuccess(res, rows);
+              }
+           });
+       }
+        else{
+           returnError(res, 'invalid session');
+       }
     });
 });
-
-router.delete('/:id', function (req, res) {
-    db.run('UPDATE Users SET active=0 WHERE user_id=?', req.params.id, function (err, lastID) {
-        if (err) {
-            res.send('database error');
-            console.error(err);
-        }
-        else {
-            var response = {'success': 1, 'change_count': this.changes};
-            res.send(JSON.stringify(response));
-        }
-    });
+router.get('/active', function(req, res){
+   verifySession(req.headers.sessionid, function(valid){
+      if(valid){
+          dataDb.all('SELECT user_id, user_name, first_name, last_name, administrator FROM Users WHERE active=1', function(err, rows){
+             if(err){
+                 returnError(res, 'database error');
+             }
+              else{
+                 returnSuccess(res, rows);
+             }
+          });
+      }
+       else{
+          returnError(res, 'invalid session');
+      }
+   });
 });
 
-router.post('/:method', function (req, res) {
-    if (req.params.method == 'addnew') {
-        var params = [req.body.username, req.body.firstname, req.body.lastname, req.body.password, req.body.admin];
-
-        db.run('INSERT INTO Users(user_name, first_name, last_name, password, administrator, active) VALUES(?,?,?,?,?,1)', params, function (err, lastID) {
-            if (err) {
-                res.send('database error');
-                console.error(err);
-            }
-            else {
-                var response = {'success': 1, 'lastid': this.lastID};
-                res.send(JSON.stringify(response));
-            }
-        });
-    }
-
-    if (req.params.method == 'login') {
-        params = [req.body.username, req.body.password];
-
-        db.get('SELECT user_id, first_name, last_name, administrator FROM Users WHERE user_name=? AND password=? AND active=1', params, function (err, row) {
-            if (err) {
-                res.send('database error');
-                console.error(err);
-            }
-            else {
-                res.send(JSON.stringify(row));
-            }
-
-        });
-    }
+/* HTTP PUT Actions */
+router.put('/changepwd/:userid', function(req, res){
+   verifySession(1234321, function(valid){
+      if(valid){
+          console.log(JSON.stringify(req.headers));
+          console.log(JSON.stringify(req.params));
+          console.log(JSON.stringify(req.body));
+          console.log(JSON.stringify(req.query));
+          var params = [req.headers.newpassword, req.headers.oldpassword, req.params.userid];
+          console.log(JSON.stringify(params));
+          dataDb.run('UPDATE Users SET password=? WHERE password=? AND user_id=?', params, function(err){
+              if (err){
+                  returnError(res, 'database error');
+              }
+              else{
+                  returnSuccess(res, {'affectedrows':this.changes, 'lastid':this.lastID});
+              }
+          })
+      }
+       else{
+          returnError(res, 'invalid session');
+      }
+   });
 });
 
 module.exports = router;
